f974489ece82c2f9723d4d7ca18a1b7b742b64a0
kuksik <nazarkukarkin@gmail.com>
2016-09-15 11:46:32 +0300

    #2 4s instead 10m

--- index.cpp
+++ index.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <algorithm>
 
+using namespace std;
+
 int prime(int num) {
   for ( int j=2; j<=num/2; j++)
   {
@@ -11,15 +13,17 @@ int prime(int num) {
   return 1;
 };
 
-std::vector<int> findDividers (int number, std::vector<int> primes) {
+int abundantNumber (int number, std::vector<int> primes) {
 
-  std::vector<int> powers;
   std::vector<int> dividers;
+  std::vector<int> array;
   dividers.push_back(1);
   int power;
   int n = number;
+  int sumOfDividers = 0;
 
   for ( int j=1; j<primes.size(); j++ ) {
+
     if ( number/2 <  primes[j] ) {
         break;
     }
@@ -30,36 +34,27 @@ std::vector<int> findDividers (int number, std::vector<int> primes) {
         power++;
     }
 
-    powers.push_back(power);
-  }
-
-  for (int i=0; i<powers.size(); i++ ) {
-    std::vector<int> array;
-    for (int k = 1; k<=powers[i]; k++) {
-        array.push_back(pow( primes[i+1], k) );
+    for (int k = 1; k<=power; k++) {
+        array.push_back(pow( primes[j], k) );
     };
 
     int divNum = dividers.size();
 
     for ( int j=0; j<divNum; j++) {
       for ( int k=0; k<array.size(); k++) {
-        if (dividers[j]*array[k] == number) break;
-        dividers.push_back(dividers[j]*array[k]);
+
+        int newDivider = dividers[j]*array[k];
+        sumOfDividers += newDivider;
+
+        if (newDivider == number) return 0;
+        if (sumOfDividers > number) return 1;
+
+        dividers.push_back(newDivider);
       }
     }
+    array.clear();
   }
 
-  return dividers;
-}
-
-int abundantNumber(int number, std::vector<int> primes) {
-  std::vector<int> dividers = findDividers(number, primes);
-
-  int sum = 0;
-  for ( int j=0; j<dividers.size(); j++) {
-      sum += dividers[j];
-  }
-  if ( sum > number ) return 1;
   return 0;
 }
 
@@ -69,49 +64,61 @@ int main(){
     cout << "type number" << endl;
     cin >> number;
 
-    std::vector<int> primes;
+    clock_t startTime = clock();
 
+    std::vector<int> primes;
+    // time = +- 0.02-0.03 s number = 28123
     for (int i=1; i<=number/2+1; i++) {
         if (prime(i)) {
           primes.push_back(i);
         }
     };
-
     int generalSum = 0;
     std::vector<int> abundantNumbers;
-
-    for ( int i=1; i<=number; i++ ) {
-        generalSum += i;
-        if ( abundantNumber(i, primes) ) {
-          abundantNumbers.push_back(i);
-        }
-    }
-
     std::vector<int> compositeNumbers;
 
+    // find all primes and all dividers for number for 1 to 28123 = 0.3s
+    for (int i=1; i <= number; i++) {
+      generalSum += i;
+      if (abundantNumber(i, primes) ) {
+        abundantNumbers.push_back(i);
+      }
+    }
+
+    // int sum = 0;
     for (int i=0; i<abundantNumbers.size(); i++) {
       for ( int j=i; j < abundantNumbers.size(); j++ ) {
         int newElem = abundantNumbers[i] + abundantNumbers[j];
         if (newElem > number) break;
 
-        std::vector<int>::iterator it;
-
-        it = find (compositeNumbers.begin(), compositeNumbers.end(), newElem);
-
-        if (it == compositeNumbers.end()) {
+        // std::vector<int>::iterator it;
+        //
+        // it = find(compositeNumbers.begin(), compositeNumbers.end(), newElem);
+        //
+        // if (it == compositeNumbers.end()) {
           compositeNumbers.push_back(newElem);
-        }
+          // sum += newElem;
+        // }
       }
     };
 
-
-    int sum = 0;
-    for ( int i=0; i < compositeNumbers.size(); i++) {
-      sum += compositeNumbers[i];
+    sort(compositeNumbers.begin(), compositeNumbers.end());
+    int sum = compositeNumbers[0];
+    for (int i =1; i<compositeNumbers.size(); i++) {
+      if (compositeNumbers[i] != compositeNumbers[i-1]) {
+        sum += compositeNumbers[i];
+      }
+      // cout << compositeNumbers[i] << endl;
     }
+
 
     cout << sum << endl;
     cout << generalSum << "   " << generalSum -  sum << endl;
 
+
+    clock_t endTime = clock();
+    clock_t clockTicksTaken = endTime - startTime;
+    double timeInSeconds = clockTicksTaken / (double) CLOCKS_PER_SEC;
+    cout << timeInSeconds << endl;
     return 0;
 }

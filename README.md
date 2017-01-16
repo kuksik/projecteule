1493e1c74ce5daa9e83ecd1d9a5cfda6a16c0100
kuksik <nazarkukarkin@gmail.co>
2017-01-16 21:31:43 +0200

    #25 1000-digit Fibonacci number

---
+++ README.md
@@ -0,0 +1,2 @@
+start and print to the console:
+  g++ index.cpp -o a.out  && ./a.out
--- index.cpp
+++ index.cpp
@@ -1,64 +1,75 @@
 #include <iostream>
-// #include <string.h>
 #include <vector>
 
 using namespace std;
 
-int main()
+int showString(vector<int>vec)
 {
-  char a[] = "82";
-  char b[] = "32";
-  char c[10];
-  int d = 0;
-  int decade = 0;
-  vector<int> res;
-
-  string sa(a);
-  string sb(b);
-  string sc(c);
-
-  if ( sa.length() < sb.length() ) 
-  {
-    sc = sa;
-    sa = sb;
-    sb = sc;
-  }
-
-  int ia = atoi(sa.c_str());
-  int ib = atoi(sb.c_str());
-
-  string ona(a);
-  string onb(b);
-  
-  for(int i=0; i<sa.length(); i++)
-  {
-    if(sb[sb.length()-i])
-    {
-      ona = sa[sa.length()-i];
-      onb = sb[sb.length()-i];
-      ia = atoi(ona.c_str());
-      ib = atoi(onb.c_str());
-
-      d = ia + ib + decade;
-      decade = 0;
-      if (d>10)
-      {
-        decade = (d - d%10)/10;
-      }
-    }
-    else
-    {
-      d = sa[sa.length()-i] + decade;
-      decade = 0;
-    }
-    res.push_back(d);
-  }
-
-  for (int i=res.size()-1; i>=0; i--)
-  {
-    cout << res[i];
+  for (int i=0; i < vec.size(); i++) {
+    cout << vec[i];
   }
   cout << endl;
   return 0;
 }
 
+vector<int>nextFibonacciNumber(vector<int>a, vector<int>b)
+{
+  vector<int> result;
+  int aLength = a.size();
+  int bLength = b.size();
+  int sum;
+  bool increaseToOne = false;
+
+  for (int i=1; i<=aLength; i++) {
+      if (bLength-i > -1) {
+        sum = a[aLength - i] + b[bLength - i];
+        if (increaseToOne) {
+          sum++;
+          increaseToOne = false;
+        };
+
+        if (sum > 9) {
+          sum = sum - 10;
+          increaseToOne = true;
+        }
+        result.push_back(sum);
+      } else {
+        sum = a[aLength - i];
+        if (increaseToOne) {
+          sum++;
+          increaseToOne = false;
+        };
+        result.push_back(sum);
+      }
+  }
+  if (increaseToOne) {
+    result.push_back(1);
+    increaseToOne = false;
+  }
+  reverse(result.begin(), result.end());
+  return result;
+}
+
+
+int main()
+{
+  vector<int> a;
+  vector<int> b;
+  vector<int> c;
+
+  a.push_back(1);
+  b.push_back(1);
+
+  int index = 2;
+
+  while(a.size() < 1000) {
+    c = nextFibonacciNumber(a, b);
+    b = a;
+    a = c;
+    index++;
+    // showString(a);
+  }
+  cout << index << endl;
+
+  return 0;
+}
